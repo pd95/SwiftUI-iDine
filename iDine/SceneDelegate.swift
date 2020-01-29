81be16c4e24a03ad74f93ea5ebdb3d270e11f4b9
Philipp <pd95@users.noreply.github.com>
2020-01-29 21:18:08 +0100

    Observable objects, environment objects, and @Published

--- iDine/SceneDelegate.swift
+++ iDine/SceneDelegate.swift
@@ -13,6 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    var order = Order()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -21,6 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
+            .environmentObject(order)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
