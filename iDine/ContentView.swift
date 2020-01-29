c5ff03e75225729456fc5107aaac39ff69a1868e
Philipp <pd95@users.noreply.github.com>
2020-01-29 19:00:03 +0100

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Philipp on 29.01.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
